407892a4409a9f89e8273378ff915369259bdf8a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-28 20:03:53 +0900

    chore: update bin-script.sh

--- scripts/bin-script.sh
+++ scripts/bin-script.sh
@@ -2,22 +2,27 @@
 set -eu -o pipefail
 
 # __USAGE__
-#   bash bin-script.sh https://github.com/hi-ogawa/misc/raw/master/scripts/bin-script.sh bin-script.sh
+#   bash bin-script.sh https://github.com/hi-ogawa/misc/raw/master/scripts/bin-script.sh
 
 # __DEPS__
 #   curl
 
 # __ARGS__
-url="$1"
-filename="$2"
+src="$1"
+filename="${2:-"$(basename "$1")"}"
 
 dst_dir="$HOME/.local/bin"
 dst_path="$dst_dir/$filename"
 
-# download to .local/bin
-echo ":: writing to $dst_path"
+echo ":: creating $dst_path"
 mkdir -p "$dst_dir"
-curl -sSfL "$url" > "$dst_path"
+
+# copy or download
+if [[ -f "$src" ]]; then
+  cp -f "$src" "$dst_path"
+else
+  curl -sSfL "$src" > "$dst_path"
+fi
 
 # make it executable
 chmod +x "$dst_path"
